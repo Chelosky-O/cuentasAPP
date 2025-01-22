5aac5f50b5e6c5395da15372f0ce5a7387dd4475
Chelosky-O <marcelofernando.mv@gmail.com>
2025-01-22 15:33:56 -0300

    lolito1

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Configuración del cliente de Supabase
-const SUPABASE_URL = import.meta.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://harguagytcmwuotfbcfo.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhhcmd1YWd5dGNtd3VvdGZiY2ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1Njk1OTksImV4cCI6MjA1MzE0NTU5OX0.GWnVbyEDzk3Th27f7cH-RTD7UgtQFWzxOiGMFJ39t70";
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   throw new Error("Las variables de entorno para Supabase no están configuradas correctamente.");
